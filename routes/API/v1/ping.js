73180b185f19adec1df99e4b615c161137fba278
kswarrior <kswarriorgh+main@gmail.com>
2026-02-28 14:51:44 +0530

    Update ping.js

--- routes/API/v1/ping.js
+++ routes/API/v1/ping.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/ping', (req, res) => {
   res.status(200).json({ status: 'ok' });
 });
 
